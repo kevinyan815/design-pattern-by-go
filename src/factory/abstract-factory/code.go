746bcf55a7e1b4d1621ec7cca5f5b97314b01539
Kevin Yan <kevinyan815@gmail.com>
2022-08-28 21:04:20 +0800

    Create code.go

---
+++ src/factory/abstract-factory/code.go
@@ -0,0 +1,75 @@
+// 示例源码运行Demo
+// 给公众号「网管叨bi叨」发生 go-factory 即可领取
+type AbstractFactory interface {
+	CreateTelevision() ITelevision
+	CreateAirConditioner() IAirConditioner
+}
+
+type ITelevision interface {
+	Watch()
+}
+
+type IAirConditioner interface {
+	SetTemperature(int)
+}
+
+type HuaWeiFactory struct{}
+
+func (hf *HuaWeiFactory) CreateTelevision() ITelevision {
+	return &HuaWeiTV{}
+}
+func (hf *HuaWeiFactory) CreateAirConditioner() IAirConditioner {
+	return &HuaWeiAirConditioner{}
+}
+
+type HuaWeiTV struct{}
+
+func (ht *HuaWeiTV) Watch() {
+	fmt.Println("Watch HuaWei TV")
+}
+
+type HuaWeiAirConditioner struct{}
+
+func (ha *HuaWeiAirConditioner) SetTemperature(temp int) {
+	fmt.Printf("HuaWei AirConditioner set temperature to %d ℃\n", temp)
+}
+
+type MiFactory struct{}
+
+func (mf *MiFactory) CreateTelevision() ITelevision {
+	return &MiTV{}
+}
+func (mf *MiFactory) CreateAirConditioner() IAirConditioner {
+	return &MiAirConditioner{}
+}
+
+type MiTV struct{}
+
+func (mt *MiTV) Watch() {
+	fmt.Println("Watch HuaWei TV")
+}
+
+type MiAirConditioner struct{}
+
+func (ma *MiAirConditioner) SetTemperature(temp int) {
+	fmt.Printf("Mi AirConditioner set temperature to %d ℃\n", temp)
+}
+
+
+func main() {
+	var factory AbstractFactory
+	var tv ITelevision
+	var air IAirConditioner
+
+	factory = &HuaWeiFactory{}
+	tv = factory.CreateTelevision()
+	air = factory.CreateAirConditioner()
+	tv.Watch()
+	air.SetTemperature(25)
+
+	factory = &MiFactory{}
+	tv = factory.CreateTelevision()
+	air = factory.CreateAirConditioner()
+	tv.Watch()
+	air.SetTemperature(26)
+}
